db23c819ebc3082576eee3ac0eba8a7888804c22
Hannes Schwanzer <hannes.schwanzer@student.hs-rm.de>
2024-05-31 00:17:12 +0200

    Commented out lsp servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -98,7 +98,7 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
+        -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -106,8 +106,8 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
-        volar = {},
+        -- tsserver = {},
+        -- volar = {},
 
         lua_ls = {
           -- cmd = {...},
